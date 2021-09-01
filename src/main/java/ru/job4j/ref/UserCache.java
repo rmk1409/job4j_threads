d97e9e6fc2af056c2fd61ee682831f0674fc79ae
Roman Pogorelov <rmk140987@gmail.com>
2021-09-01 14:51:54 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -0,0 +1,27 @@
+package ru.job4j.ref;
+
+import org.apache.http.annotation.NotThreadSafe;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return users.values().stream()
+                .map(user -> User.of(user.getName()))
+                .toList();
+    }
+}
