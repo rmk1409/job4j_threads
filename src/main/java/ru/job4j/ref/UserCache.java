0f2256d7862786c1f67d99457039e906d97b9701
Roman Pogorelov <rmk140987@gmail.com>
2021-09-02 19:49:59 +0300

    Changed @ for 4. Thread без общих ресурсов [#267919 #197510]

--- src/main/java/ru/job4j/ref/UserCache.java
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -1,12 +1,12 @@
 package ru.job4j.ref;
 
-import net.jcip.annotations.NotThreadSafe;
+import net.jcip.annotations.ThreadSafe;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@NotThreadSafe
+@ThreadSafe
 public class UserCache {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private final AtomicInteger id = new AtomicInteger();
