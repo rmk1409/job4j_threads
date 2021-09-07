edd8213245720c689f62d05976cdb2a2443aaf4f
Roman Pogorelov <rmk140987@gmail.com>
2021-09-07 06:57:37 +0300

    Simplified 0. CAS - операции [#6859 #198462]

--- src/test/java/ru/job4j/CASCountTest.java
+++ src/test/java/ru/job4j/CASCountTest.java
@@ -8,21 +8,31 @@ public class CASCountTest {
     @Test
     public void test() throws InterruptedException {
         CASCount count = new CASCount();
-        Thread[] threads = new Thread[10];
-        for (int i = 0; i < threads.length; i++) {
-            threads[i] = new Thread(() -> {
-                for (int j = 0; j < 10_000; j++) {
-                    count.increment();
-                    Thread.yield();
-                }
-            });
+        Thread thread1 = new Thread(new MyRunnable(count));
+        Thread thread2 = new Thread(new MyRunnable(count));
+        Thread thread3 = new Thread(new MyRunnable(count));
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread1.join();
+        thread2.join();
+        thread3.join();
+        assertEquals(30_000, count.get());
+    }
+
+    static class MyRunnable implements Runnable {
+        private CASCount count;
+
+        public MyRunnable(CASCount count) {
+            this.count = count;
         }
-        for (Thread thread : threads) {
-            thread.start();
+
+        @Override
+        public void run() {
+            for (int j = 0; j < 10_000; j++) {
+                count.increment();
+                Thread.yield();
+            }
         }
-        for (Thread thread : threads) {
-            thread.join();
-        }
-        assertEquals(100_000, count.get());
     }
 }
